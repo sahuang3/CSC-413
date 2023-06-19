43baaee362f4e5cf1384c97604a3fbbff5761d00
sahuang3 <89601319+sahuang3@users.noreply.github.com>
2023-06-19 01:14:49 -0700

    Delete Projects/Design Pattern directory

--- Projects/Design Pattern/LookGenerator.java
+++
@@ -1,5 +0,0 @@
-// Interface for Looking in rooms
-public interface LookGenerator {
-
-    void look(String room);
-}
--- Projects/Design Pattern/SimpleResponse.java
+++
@@ -1,7 +0,0 @@
-// Response if it's light or dark
-public class SimpleResponse implements LookGenerator{
-
-    public void look(String room){
-        System.out.println(room);
-    }
-}
